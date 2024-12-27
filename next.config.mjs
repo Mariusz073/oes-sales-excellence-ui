2b016cef141ee0407cc1814df12dac189705051f
Mariusz Kram <mariusz@krambit.pl>
2024-12-27 16:59:21 +0100

    Remove additional directory inclusion configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,4 @@
-// next.config.mjs
-import CopyPlugin from 'copy-webpack-plugin';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push(
-        new CopyPlugin({
-          patterns: [
-            {
-              from: 'team_reports',
-              to: 'team_reports'
-            },
-            {
-              from: 'json_reports',
-              to: 'json_reports'
-            }
-          ],
-        })
-      );
-    }
-    return config;
-  }
-}
+const nextConfig = {}
 
 export default nextConfig;
