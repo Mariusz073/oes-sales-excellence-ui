b3f0437e276584bbe2784fcb60b02f51db62124b
Mariusz Kram <mariusz@krambit.pl>
2024-12-27 16:21:04 +0100

    Include team and json reports paths in the build process

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Copy additional files/directories to the server build
+  serverComponentsExternalPackages: ['team_reports', 'json_reports'],
+  // Explicitly include files/directories in the build
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.copy = config.copy || [];
+      config.copy.push(
+        {
+          from: 'team_reports',
+          to: 'team_reports'
+        },
+        {
+          from: 'json_reports',
+          to: 'json_reports'
+        }
+      );
+    }
+    return config;
+  }
+}
 
 export default nextConfig;
