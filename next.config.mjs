f3dbe48fd65fcdd6e1895d882c37fe696453b649
Mariusz Kram <mariusz@krambit.pl>
2024-12-27 16:27:58 +0100

    Build the app correctly

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,23 @@
+// next.config.mjs
+import CopyPlugin from 'copy-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Copy additional files/directories to the server build
-  serverComponentsExternalPackages: ['team_reports', 'json_reports'],
-  // Explicitly include files/directories in the build
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.copy = config.copy || [];
-      config.copy.push(
-        {
-          from: 'team_reports',
-          to: 'team_reports'
-        },
-        {
-          from: 'json_reports',
-          to: 'json_reports'
-        }
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: 'team_reports',
+              to: 'team_reports'
+            },
+            {
+              from: 'json_reports',
+              to: 'json_reports'
+            }
+          ],
+        })
       );
     }
     return config;
